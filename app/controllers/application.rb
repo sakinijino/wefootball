1bc368fb3549009b2252fad6ef97b7e1fa4ee0c8
csdt <csdt@gmail.com>
2008-01-20 04:46:09 +0000

    csdt

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,8 +3,11 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  include AuthenticatedSystem
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'dec45b9c94def5f51038e484e0d32ad6'
+  # TODO - this will be uncommented once we explain sessions
+  # in iteration 5.
+  # protect_from_forgery
+  # :secret => 'dd92c128b5358a710545b5e755694d57'
 end
