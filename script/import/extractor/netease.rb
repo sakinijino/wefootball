bca376be163528e19d2ec699ee3c7a223fe685fc
saki <sakinijino@gmail.com>
2010-12-09 10:37:11 +0800

    better matches import

--- script/import/extractor/netease.rb
+++ script/import/extractor/netease.rb
@@ -1,10 +1,25 @@
 require 'nokogiri'
 
-def netease_src_uri(year, month, day, type)
-  "http://goal.sports.163.com/#{MATCH_TYPE[type]}/schedule/#{year}#{month}#{day}.html"
+NETEASE_MATCH_TYPE_MAPPING = {
+  :england => '8/', #英超
+  :spain => '23/', #西甲
+  :italy => '21/', #意甲
+  :germany => '22/', #德甲
+  :france => '24/', #法甲
+  :euro => '5/', #冠军联赛
+  :eurolg => '6/', #欧洲联赛
+  :all => ''
+}
+
+def netease_src_uri(date, type)
+  year, month, day = date.strftime("%Y,%m,%d").split(',')
+  "http://goal.sports.163.com/#{NETEASE_MATCH_TYPE_MAPPING[type]}schedule/#{year}#{month}#{day}.html"
 end
 
-def netease_extract_matches(doc, year, month, day)
+def netease_extract_matches(date, type)
+  doc = Nokogiri::HTML(open(netease_src_uri(date, type)), 'utf8')
+
+  year, month, day = date.strftime("%Y,%m,%d").split(',')
   rt = []
   return rt if doc.css("#calender_#{year}#{month}#{day}").length == 0
 
@@ -17,7 +32,7 @@ def netease_extract_matches(doc, year, month, day)
       :guest_name => '',
       :host_goal => nil,
       :guest_goal => nil,
-      :start_time => DateTime.new(year.to_i,month.to_i,day.to_i)
+      :start_time => date
     }
     match[:host_name] = tr.css("span.c1 a").first.content
     match[:guest_name] = tr.css("span.c2 a").first.content
--- script/import/matches.rb
+++ script/import/matches.rb
@@ -5,33 +5,58 @@ require 'open-uri'
 require File.dirname(__FILE__) + '/extractor/netease'
 
 MATCH_TYPE = {
-  :england => '8', #英超
-  :spain => '21', #西甲
-  :italy => '23', #意甲
-  :germany => '22', #德甲
-  :euro => '5', #冠军联赛
+  :england => :england, #英超
+  :spain => :spain, #西甲
+  :italy => :italy, #意甲
+  :germany => :germany, #德甲
+  :france => :france, #法甲
+  :euro => :euro, #冠军联赛
+  :eurolg => :eurolg, #欧洲联赛
+  :all => :all
 }
-
-date = {
-  :year => ARGV[0],
-  :month => ARGV[1],
-  :day => ARGV[2]
-}
-
-matches = []
 
 begin
-  MATCH_TYPE.each do |type,w|
-    doc = Nokogiri::HTML(open(src_uri(date[:year], date[:month], date[:day], type)), 'utf8')
-    matches << extract_matches(doc, date[:year], date[:month], date[:day])
-    matches.flatten!
-    sleep(2)
+  if ARGV.length >= 4
+    dates = [DateTime.parse(ARGV[-2])]
+    type = ARGV[-1].downcase.to_sym
+  elsif ARGV.length == 3
+    dates = [DateTime.parse(ARGV[-1])]
+    type = :all
+  else
+    dates = [DateTime.yesterday, DateTime.now, DateTime.now + 7.days]
+    type = :all
   end
 rescue
-  puts "Remote Extract Error!"
- puts
+  puts "Usage: ruby script\/runner -e ENV script\import\matches.rb [DateTime] [MatchType]"
+  exit
 end
 
+#####################
+# Extract Matches
+#####################
+matches = []
+begin
+  dates.each do |date|
+    puts "* #{date.strftime("%Y-%m-%d")}:"
+    puts "  Import #{type} matches from #{src_uri(date, type)}."
+    tmp = extract_matches(date, type)
+    puts "  Extract #{tmp.length} matches."
+    matches << tmp
+    matches.flatten!
+    sleep(2)
+    puts
+  end
+rescue
+  puts "* Remote Extract Error!"
+  puts
+end
+
+puts "-----------------------"
+puts
+
+#####################
+# Import Matches
+#####################
 new_teams = []
 new_matches = []
 update_matches = []
@@ -49,34 +74,34 @@ if matches.length > 0
       end
     end
   rescue
-    puts "Import Error!"
+    puts "* Import Error!"
     puts
   end
 
-  puts "New Teams:"
+  puts "* New Teams:"
   new_teams.each do |t|
-    puts t.name
+    puts "  #{t.name}"
   end
   puts
 
-  puts "New Matches:"
+  puts "* New Matches:"
   new_matches.each do |m|
     if m.host_team_goal.nil? || m.guest_team_goal.nil?
-      puts "#{m.host_team_name} V.S. #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
+      puts "  #{m.host_team_name} V.S. #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
     else
-      puts "#{m.host_team_name} #{m.host_team_goal} : #{m.guest_team_goal} #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
+      puts "  #{m.host_team_name} #{m.host_team_goal} : #{m.guest_team_goal} #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
     end
   end
   puts
 
-  puts "Update Matches:"
+  puts "* Update Matches:"
   update_matches.each do |m|
     if m.host_team_goal.nil? || m.guest_team_goal.nil?
-      puts "#{m.host_team_name} V.S. #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
+      puts "  #{m.host_team_name} V.S. #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
     else
-      puts "#{m.host_team_name} #{m.host_team_goal} : #{m.guest_team_goal} #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
+      puts "  #{m.host_team_name} #{m.host_team_goal} : #{m.guest_team_goal} #{m.guest_team_name} at #{m.start_time.strftime("%Y-%m-%d %H:%M")}"
     end
   end
 else
-  puts "Import Nothing."
+  puts "* Import Nothing."
 end
